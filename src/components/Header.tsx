24878d65ec5f51c22bfca90349992191c8700057
Christian Larsen <christi9nl9rsen@gmail.com>
2026-04-09 19:38:50 -0500

    navbar Demo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
 export default function Header(){
     return(
-        <div className="header">
-            <h1>Business Partners of Anna</h1>
-        </div>
+        <>
+            <div className="header">
+                <div className={"headerText"}>
+                    <h1>Business Partners of Anna</h1>
+                </div>
+            </div>
+            <div className={"navBar"}>
+                <ul>
+                    <li><a href={"#"}><button>Placeholder</button></a></li>
+                    <li><a href={"#"}><button>Placeholder</button></a></li>
+                    <li><a href={"#"}><button>Placeholder</button></a></li>
+                    <li><a href={"#"}><button>Placeholder</button></a></li>
+                </ul>
+            </div>
+        </>
     );
 }
